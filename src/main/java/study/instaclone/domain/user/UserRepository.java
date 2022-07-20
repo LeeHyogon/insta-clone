431db03f38c649a6710aed688c72f8cfaa9fa51c
이효곤 <LeeHyogon>
2022-07-21 02:30:52 +0900

    회원가입 기능 구현 문제1: 웹페이지 경로 login 고정 이유?
    남은 것: 시큐리티 적용

---
+++ src/main/java/study/instaclone/domain/user/UserRepository.java
@@ -0,0 +1,12 @@
+package study.instaclone.domain.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findUserByEmail(String email);
+    Optional<User> findUserById(Long id);
+}
